5b78af1ea52a03596cedef62c9825b0e9dc04a17
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-06 22:24:33 +0300

    add Example_Sum

---
+++ Example_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿int A = 79729;
+int B = 383;
+int result = A + B;
+// Console.WriteLine("Введите первое слагаемое: ");
+// int A = Console.ReadLine();
+// Console.Write("A = " + A);
+// Console.WriteLine("Введите второе слагаемое: ");
+// int B = Console.ReadLine();
+Console.WriteLine(result);
