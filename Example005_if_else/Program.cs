1fa71d7bc8681f8bc443010580931b74a5fdbd28
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-06 23:40:01 +0300

    add Example if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else {
+    Console.Write("Привет,"+ username +"!");
+}
