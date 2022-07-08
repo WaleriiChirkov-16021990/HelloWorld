823b80b6447d31d4915d53a060a4a361ce2d7798
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-08 22:45:22 +0300

    add Example_sqrt

---
+++ Example _sqrt/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число" );
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Первое число = " + a );
+Console.WriteLine("Введите второе число" );
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Второе число = " + b );
+
+if( b*b == a )
+{
+
+    Console.WriteLine( "Первое число является квадратом второго числа" );
+
+}
+else
+{
+    Console.WriteLine( "Первое число не является квадратом второго числа" );
+}                                                  
