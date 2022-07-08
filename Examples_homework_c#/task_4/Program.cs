7314eaf6392ccf60b0d971fff1bcef4605b16e80
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-09 00:06:37 +0300

    add Exsamples_homework_c#_task_4

---
+++ Examples_homework_c#/task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the third number: ");
+int c = int.Parse(Console.ReadLine());
+
+Console.WriteLine("First number = " + a);
+Console.WriteLine("Second number = " + b);
+Console.WriteLine("The third number = " + c);
+
+int max = a;
+
+    if( b>max ){
+        max = b;
+        Console.WriteLine("Max = Second_Number = " + max);
+    }
+      else if( c>max ){
+        max=c;
+        Console.WriteLine("Max = The third = " + max);
+    }
+      else{
+        Console.WriteLine("Max = First_Number =  " + max);
+    }
