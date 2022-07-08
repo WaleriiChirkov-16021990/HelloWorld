78fb3c7d850fa279a5fa5f2f5051f7247a12e6ef
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-08 23:20:00 +0300

    add Exsamples_homework_c#

---
+++ Examples_homework_c#/task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("First number = " + a);
+Console.WriteLine("Second number = " + b);
+
+int max = a;
+
+if( b>max ){
+    max = b;
+    Console.WriteLine("Max = Second_Number = " + max);
+}
+else{
+    Console.WriteLine("Max = First_Number =  " + max);
+}
