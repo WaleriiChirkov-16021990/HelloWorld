a7d6186e947a1195eab9a253fe722296fe66056e
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-09 19:10:37 +0300

    add Exsamples_homework_c#_task_6

---
+++ Examples_homework_c#/task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a%2==0){
+    Console.WriteLine("Number : "+ a + " является чётным !");
+}
+else {
+        Console.WriteLine("Number : "+ a + " является не чётным !");
+
+}
+
