4ef7b74797993b680b69a3c9506a45bd518b4b46
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-10 17:57:34 +0300

    add Example_void_FindArrayI

---
+++ Examples_void/FindArrayI/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int FindArrayI(int  Array,int find)
+{
+int [] array = Array[Array.Length];
+int index = 0;
+// int count = -1;
+int size = Array.Length;
+
+while( index< size ) {
+    int count = -1;
+    if(Array[index]==find){
+        count = index;
+        break;
+    }
+    index++;
+
+}
+return count;
+
+}
+
+int [] Array  = {1,4,6,1,7,8,23,46,78,90};
+int count =  FindArrayI(Array, 46);
+Console.WriteLine(count);
