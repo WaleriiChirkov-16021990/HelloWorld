293d3908903ee97ac919f0042165661bc7383757
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-11 22:15:12 +0300

    add Example_reverse array & first version SortArray

---
+++ Examples_void/ReverseArray/Program.cs
@@ -0,0 +1,18 @@
+﻿void ReverseArray(int [] array){
+    int size = array.Length;
+    int index = 0;
+    int x=0;
+
+   
+    while ( index < size-index-1){
+        x=array[index];
+        array[index]=array[size-index-1];
+        array[size-index-1]=x;
+        index++;
+    }
+    Console.WriteLine(string.Join("," , array ));
+}
+
+
+// int [] Numbers = {14,34,47,345,13,0,5,67,100,400};
+// ReverseArray(Numbers);
---
+++ Examples_void/SortArray/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+void SortArray(int [] Array,int sort){
+int size = Array.Length;
+int index = 0;
+// int min=0;
+int max = 0;
+int x = 0;
+
+if(sort == 1){
+     while(index<size){
+    if(Array[index]>max){
+        max = Array[index];
+        x = Array[index];
+        for (int i = index+1; i <= size-1; i++ ){
+            Array[i-1] = Array[i];
+        }
+        Array[size-1]=x;
+        }
+           
+    }
+    index++;
+  }
+  Console.Write(string.Join(",", Array));
+
+    // while (index<size){
+    //  index =1;
+    //  if ( Array[index]< min){
+    //     Array[index]= min;
+    //     x=Array[index];
+    
+    //     for(int i = index-1; i > 0; i=i-1){
+    //         Array[i]=Array[i+1];
+    //                 }
+    //     Array[0]=x;
+       
+    //  }
+    //  return;
+    // }
+    // Console.Write(string.Join(",", Array));
+    // Console.WriteLine(min);
+}
+// else {
+//     while (index<size){
+//      index =1;
+//      if ( Array[index]> max){
+//         Array[index]= max;
+//         x=Array[index];
+    
+//         for(int i = index-1; i > 0; i=i-1){
+//             Array[i]=Array[i+1];
+//                     }
+//         Array[size-1]=x;
+       
+//      }
+//      return;
+//     }
+//     Console.Write(string.Join(",", Array));
+//     Console.WriteLine(max);
+
+// }
+
+
+}
+
+int [] Numbers = {5,1,10,67};
+SortArray(Numbers,1);
+
