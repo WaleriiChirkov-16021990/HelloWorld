0979d121989ac1d0cff82f0bc977b917e3be1c20
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-12 21:10:54 +0300

    add Example_reverse array & work version SortArray v 1.1 delete comment

--- Examples_void/SortArray/Program.cs
+++ Examples_void/SortArray/Program.cs
@@ -1,13 +1,13 @@
 ﻿
 using System;
 void SortArray(int [] Array,int min, int max){
-    // int [] Array = Array;
+
 int [] sortedArray = QuickSort(Array, 0, Array.Length - 1);
 
 
 Console.Write($"Sorted array : {string.Join(",", sortedArray )}");
 
-// Console.ReadLine();
+ Console.ReadLine();
 
 
 int [] QuickSort(int [] Array , int minIndex, int maxIndex)
@@ -57,81 +57,3 @@ void Swap(ref int leftItem, ref int rightItem)
 
 int [] Numbers = {5,1,10,67,6,4,0,99,95,84,30};
 SortArray (Numbers,0,7);
-
-
-
-
-
-
-
-
-// while(index<size){
-//     if(Array[index]> max){
-//         max = Array[index];
-//     }
-//     index++;
-// }
-// Array[0]=max;
-
-// if(sort == 1){
-//      while(index<size){
-//          if()
-
-
-
-
-    // if(Array[index]>max){
-    //     max = Array[index];
-    //     x = Array[index];
-    //     for (int i = index+1; i <= size-1; i++ ){
-    //         Array[i-1] = Array[i];
-    //     }
-    //     Array[size-1]=x;
-    //     }
-           
-//     }
-//     index++;
-//   }
-//   Console.Write(string.Join(",", Array));
-
-    // while (index<size){
-    //  index =1;
-    //  if ( Array[index]< min){
-    //     Array[index]= min;
-    //     x=Array[index];
-    
-    //     for(int i = index-1; i > 0; i=i-1){
-    //         Array[i]=Array[i+1];
-    //                 }
-    //     Array[0]=x;
-       
-    //  }
-    //  return;
-    // }
-    // Console.Write(string.Join(",", Array));
-    // Console.WriteLine(min);
-// }
-// else {
-//     while (index<size){
-//      index =1;
-//      if ( Array[index]> max){
-//         Array[index]= max;
-//         x=Array[index];
-    
-//         for(int i = index-1; i > 0; i=i-1){
-//             Array[i]=Array[i+1];
-//                     }
-//         Array[size-1]=x;
-       
-//      }
-//      return;
-//     }
-//     Console.Write(string.Join(",", Array));
-//     Console.WriteLine(max);
-
-// }
-
-
-// }
-
-
