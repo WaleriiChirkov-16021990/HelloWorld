9c6db20b72fe5d27a3c326f642d8e9439ff00891
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-07-17 16:15:49 +0300

    Add_Examples_homework_Seminar_3\task_Lession and Examples_void\SortArray_min_max

--- Examples_void/SortArray/Program.cs
+++ Examples_void/SortArray/Program.cs
@@ -1,59 +1,60 @@
 ﻿
 using System;
-void SortArray(int [] Array,int min, int max){
-
-int [] sortedArray = QuickSort(Array, 0, Array.Length - 1);
-
-
-Console.Write($"Sorted array : {string.Join(",", sortedArray )}");
-
- Console.ReadLine();
-
-
-int [] QuickSort(int [] Array , int minIndex, int maxIndex)
+void SortArray(int[] Array, int min, int max)
 {
-    if (minIndex>=maxIndex)
+
+    int[] sortedArray = QuickSort(Array, 0, Array.Length - 1);
+
+
+    Console.Write($"Sorted array : {string.Join(",", sortedArray)}");
+
+    Console.ReadLine();
+
+
+    int[] QuickSort(int[] Array, int minIndex, int maxIndex)
     {
+        if (minIndex >= maxIndex)
+        {
+            return Array;
+        }
+        int pivotIndex = GetPivotIndex(Array, minIndex, maxIndex);
+
+        QuickSort(Array, minIndex, pivotIndex - 1);
+
+        QuickSort(Array, pivotIndex + 1, maxIndex);
+
         return Array;
     }
-    int pivotIndex = GetPivotIndex(Array, minIndex, maxIndex) ;
 
-    QuickSort(Array,minIndex,pivotIndex-1);
-
-    QuickSort(Array,pivotIndex+1,maxIndex);
-
-return Array;
-}
-
-int GetPivotIndex(int [] Array, int minIndex, int maxIndex )
-{
-    int pivot =minIndex-1;
-
-    for (int i = minIndex; i <= maxIndex; i++)
+    int GetPivotIndex(int[] Array, int minIndex, int maxIndex)
     {
-        if (Array[i]<Array[maxIndex])
+        int pivot = minIndex - 1;
+
+        for (int i = minIndex; i <= maxIndex; i++)
         {
-            pivot++;
-            Swap(ref Array[pivot], ref Array[i]);
+            if (Array[i] < Array[maxIndex])
+            {
+                pivot++;
+                Swap(ref Array[pivot], ref Array[i]);
+            }
         }
+
+        pivot++;
+
+        Swap(ref Array[pivot], ref Array[maxIndex]);
+
+        return pivot;
     }
 
-     pivot++;
-     
-     Swap(ref Array[pivot], ref Array[maxIndex]);
+    void Swap(ref int leftItem, ref int rightItem)
+    {
+        int x = leftItem;
+        leftItem = rightItem;
+        rightItem = x;
 
-    return pivot;
-}
-
-void Swap(ref int leftItem, ref int rightItem)
-{
-            int x = leftItem;
-            leftItem= rightItem;
-            rightItem= x ;
-
-}
+    }
 
 }     // delete } for start after version//
 
-int [] Numbers = {5,1,10,67,6,4,0,99,95,84,30};
-SortArray (Numbers,0,7);
+int[] Numbers = { 5, 1, 10, 67, 6, 4, 0, 99, 95, 84, 30 };
+SortArray(Numbers, 0, 4);
---
+++ Examples_void/SortArray_min_max_Lesson_3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+int[] arr = { 1, 4, 2, 7, 0, 12, 56, 2, 5, 7, 23 };
+PrintArray(arr);
+SelectionArray_Min_Max(arr);
+PrintArray(arr);
+SelectionArray_Max_Min(arr);
+PrintArray(arr);
+
+
+
+
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (var i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+
+    Console.WriteLine();
+}
+
+void SelectionArray_Min_Max(int[] Array)
+{
+    for (int i = 0; i < Array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < Array.Length; j++)
+        {
+            if (Array[j] < Array[minPosition]) minPosition = j;
+
+        }
+
+        int tempPosition = Array[i];
+        Array[i] = Array[minPosition];
+        Array[minPosition] = tempPosition;
+    }
+}
+
+void SelectionArray_Max_Min(int[] ARRAY)
+{
+    for (int i = 0; i < ARRAY.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (var j = i + 1; j < ARRAY.Length; j++)
+        {
+            if (ARRAY[j] > ARRAY[maxPosition]) maxPosition = j;
+        }
+        int tempPosition = ARRAY[i];
+        ARRAY[i] = ARRAY[maxPosition];
+        ARRAY[maxPosition] = tempPosition;
+    }
+}
