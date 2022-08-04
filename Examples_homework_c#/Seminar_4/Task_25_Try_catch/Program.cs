62e86de57859dccc9056524b94114441e170d0a6
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-08-04 23:36:38 +0300

    Add Task 25 Try_Catch

---
+++ Examples_homework_c#/Seminar_4/Task_25_Try_catch/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+
+*/
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Программа принимает на вход два числа и возводит число A в натуральную степень B");
+    double number;
+    int degree;
+    try
+    {
+        Console.Write("Введите число : ");
+        number = double.Parse(Console.ReadLine());
+        Console.Write("Введите степень: ");
+        degree = int.Parse(Console.ReadLine());
+
+    }
+    catch (Exception)
+    {
+        Console.Write("Недопустимое значение! Введите число и натуральную степень!! (1,2,3,4....) ");
+        Console.ReadLine();
+        continue;
+    }
+    Console.WriteLine(" Число " + number + " в степени " + degree + " = " + Math.Round(Math.Pow(number, degree), 3));
+    Console.ReadLine();
+}
+
