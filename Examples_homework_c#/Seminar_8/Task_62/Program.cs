01a4173fad2d514d6cce744c315bbfe515b59244
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-08-21 16:28:45 +0300

    Edit homework Task_62 Seminar 8

--- Examples_homework_c#/Seminar_8/Task_62/Program.cs
+++ Examples_homework_c#/Seminar_8/Task_62/Program.cs
@@ -1,18 +1,9 @@
-﻿
-/*
-Задача 62. Заполните спирально массив 4 на 4.
-
+﻿/*Задача 62. Заполните спирально массив 4 на 4.
 Например, на выходе получается вот такой массив:
-
 1  2  3  4
 12 13 14 5
 11 16 15 6
-10 9  8  7
-*/
-
-
-using System;
-
+10 9  8  7 */
 Console.Clear();
 System.Console.WriteLine("Данная программа заполняет спиралью массив 4х4 и любого другого размера!");
 System.Console.Write("Введите количество строк и столбцов генерируемой целочисленной матрици \nввод через пробел: ");
@@ -24,51 +15,39 @@ PrintArray(rndArray1);
 int[,] FillArray(string[] input)
 {
     int[,] array = new int[int.Parse(input[0]), int.Parse(input[1])];
-    int sizeItem = int.Parse(input[0]) * int.Parse(input[1]);
 
-    for (int k = 0, item = 1; k <= int.Parse(input[0]) && item <= int.Parse(input[0]) * int.Parse(input[1]); k++)
+    for (int k = 0, item = 0;
+        k <= int.Parse(input[0]) / 2; k++)
     {
-
-        for (int j = k, i = k; j < int.Parse(input[1]) - 1 - k; j++)
+        for (int j = k, i = k;
+            j < int.Parse(input[1]) - 1 - k; j++)
         {
-            if (array[i, j] == 0)
-            {
-                array[i, j] = item;
-            }
-            item++;
+            GetItemArray(ref array[i, j], ref item);
         }
-        for (int j = int.Parse(input[1]) - 1 - k, i = k; i < int.Parse(input[0]) - 1 - k; i++)
+        for (int j = int.Parse(input[1]) - 1 - k, i = k;
+                 i < int.Parse(input[0]) - 1 - k; i++)
         {
-            if (array[i, j] == 0)
-            {
-                array[i, j] = item;
-            }
-            item++;
+            GetItemArray(ref array[i, j], ref item);
         }
-
-
-        for (int i = int.Parse(input[0]) - 1 - k, j = int.Parse(input[1]) - 1 - k; j >= k; --j)
+        for (int i = int.Parse(input[0]) - 1 - k,
+                 j = int.Parse(input[1]) - 1 - k;
+                 j >= k; --j)
         {
-            if (array[i, j] == 0)
-            {
-                array[i, j] = item;
-            }
-            item++;
+            GetItemArray(ref array[i, j], ref item);
         }
-
-        for (int i = int.Parse(input[0]) - 2 - k, j = k; i >= k + 1; --i)
+        for (int i = int.Parse(input[0]) - 2 - k, j = k;
+             i >= k + 1; --i)
         {
-            if (array[i, j] == 0)
-            {
-                array[i, j] = item;
-            }
-            item++;
+            GetItemArray(ref array[i, j], ref item);
         }
     }
-
     return array;
 }
 
+void GetItemArray(ref int element, ref int item)
+{
+    if (element == 0) element = ++item;
+}
 
 void PrintArray(int[,] array)
 {
